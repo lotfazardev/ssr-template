1bf10efd321c21fbb72ac83c024f0d67fed1e51b
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-10-25 22:19:14 +0330

    SYNC index.js with App.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// backEnd dependencies
 const express = require("express");
 const app = express();
+// frontEnd dependencies
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const App = require('./client/App').default;
 
 app.get("/", (req, res) => {
-
+    const content = renderToString(<App />);
+    res.send(content)
 });
 
 app.listen(3000, ()=>{
